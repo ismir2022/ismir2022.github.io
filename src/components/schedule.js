eedacc843f2edf7a20c22d03f8588fb456f40a4a
Ajay Srinivasamurthy <ajays.murthy@upf.edu>
2022-09-21 01:54:40 +0530

    Update schedule.js

--- src/components/schedule.js
+++ src/components/schedule.js
@@ -12,9 +12,9 @@ const Schedule = () => {
       <div className="space-y-3">
         <div className="flex justify-center md:col-start-1 md:col-end-2 md:place-self-center ">
          <p>This is a tentative schedule of the ISMIR program this year in December 2022, and it may change in the future. 
-            In addition to the main conference program below between 4-8 Dec, there are also several {" "}
+            In addition to the main conference program below between 04-08 December, there are also several {" "}
             <span className="text-[#d83616] "> <a href="https://ismir2022.ismir.net/program/satellites" target="_blank">satellite events</a></span>
-             {" "}planned around ISMIR 2022 from 02 Dec to 16 Dec, 2022. Please watch this space for updates!</p>
+             {" "}planned around ISMIR 2022 from 02-16 December, 2022. Please watch this space for updates!</p>
         </div>
       </div>
       <div className="space-y-3">
