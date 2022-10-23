767735e55b08dd47d82120f375afa4d9f90891df
Ajay Srinivasamurthy <ajays.murthy@upf.edu>
2022-10-23 16:58:45 +0530

    Update satellites.js

--- src/components/satellites.js
+++ src/components/satellites.js
@@ -86,7 +86,7 @@ const Satellites = () => {
           </span>
         </p>
         <br></br>
-        <p className="text-xl font-bold">MusicHackDay India 2022</p>
+        <p className="text-xl text-[#d83616] font-bold">MusicHackDay India 2022</p>
         <p className="text-lg font-bold">Dec 10-11, 2022 in Bengaluru, India</p>
         <p className="text-lg">
           <span className="text-[#d83616]">
