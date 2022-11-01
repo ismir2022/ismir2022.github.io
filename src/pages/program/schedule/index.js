a3bef9539f37f42d79f5fc12f655199e1e54bc77
Ajay Srinivasamurthy <ajaysri@amazon.com>
2022-11-01 16:49:41 +0530

    Reverting to an older history
    This reverts commit 2f57755a7829f59365c8c3a4ea337122a68dfa3d.

--- src/pages/program/schedule/index.js
+++ src/pages/program/schedule/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Layout from '../../../components/layout'
-import Schedule from '../../../components/schedule_test'
+import Schedule from '../../../components/schedule'
 
 const Index = () => {
   return (
