d4d75e283f24020661d89004c823b6f3bf6677b0
Siddharth Gururani <siddgururani@gatech.edu>
2022-11-01 09:35:33 -0700

    Add lbd program content

--- src/components/lbdprogram.js
+++ src/components/lbdprogram.js
@@ -11,9 +11,31 @@ const Latebreaking = () => {
 
       <div className="space-y-3">
         <p className="text-lg">
-          Accepted LBD submissions and detailed LBD program will be added shortly!
+          The Late-breaking/Demo (LBD) session is a forum for sharing prototype systems, initial concepts, and early results which may have not yet fully matured but are of interest to the Music-IR community. 
+          It is also a great entry point for people who are new to ISMIR to showcase their preliminary work and receive early feedback from fellow researchers. 
+          Attendees of the LBD can interact with demos or discuss their thoughts on the latest developments in the field.
         </p>
       </div>
+      
+        <p className="text-xl text-[#d83616] font-bold">Hybrid Conference</p>
+          Similar to the main paper track, this year’s LBD will be held in a hybrid format, i.e., physical and remote. 
+          There will be two back-to-back sessions:
+            <ol>
+            <li>Physical session: Dec 8, 2022 (Thu), 3.30 pm - 5.30 pm</li>
+            <li>Virtual session: Dec 8, 2022 (Thu), 5.30 pm - 7.00 pm</li>
+            </ol>
+        </p>
+  
+        <p className="text-xl text-[#d83616] font-bold">Open Session</p>
+          This year’s local LBD session is also an open session for students and independent researchers of the local community. 
+          The session will enable participants to attend the late-breaking/demo session of the conference and interact with students, experts and senior members of the ISMIR research community. 
+          This open session is a free registration event.
+        </p>
+        
+        <p className="text-lg">
+          Accepted LBD submissions and detailed LBD program will be added shortly!
+        </p>
+        
     </div>
   );
 };
