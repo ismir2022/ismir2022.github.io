71732a9181d1189ad62ae7d49fd8ceb3d784fd2a
Ajay Srinivasamurthy <ajays.murthy@upf.edu>
2022-11-05 13:15:21 +0530

    Create dhaatu.js

---
+++ src/components/dhaatu.js
@@ -0,0 +1,100 @@
+import React from "react";
+
+const Dhaatu = () => {
+  return (
+    <div className="space-y-5">
+      <div>
+        <h1 className="text-[#d83616] font-bold md:text-3xl text-2xl mt-5">
+          Mālavikāgnimitram by Dhaatu Puppet Theater
+        </h1>
+        <h2>
+          Based on Mahakavi Kalidasa's Play
+        </h2>
+      </div>
+      <div className="space-y-3">
+        <p className="text-xl font-bold">Dec 5, 2022 (Mon), 5.30 pm - 7.00 pm at Satish Dhawan Auditorium, IISc</p>
+  
+        <p className="text-xl font-bold">About Mãlavikãgnimitram</p>
+  Mãlavikãgnimitram is a Sanskrit play by Mahakavi Kalidasa. Set in Sunga Period, it tells the story of Agnimitra, 
+    the Shunga Emperor of the Vidisha Kingdom, who falls in love with a maiden named Malavika. Malavika is known 
+    to be an extraordinary dancer and a sakhi of the chief queen Dhaarini. The plot unfolds when the king witnesses 
+    her dance in the court. The sutradhara of the show is Gautama, the Vidushaka and the minister and close friend 
+    of king Agnimitra. He hatches out all the plots for King Agnimitra to meet his love Malavika and escape the 
+    wrath of queen Iravati and censure of queen Dharini. Poet Kalidasa beautifully portrays the characters and 
+    unravels a plot full of fateful incidents with humorous angles.
+    
+    <p className="text-xl font-bold">About Dhaatu Puppet Theater</p>
+        <p className="text-lg">
+        Dhaatu Puppet theater is a leading classical puppet theater of India. Research of over 15 
+  years on the methodologies of construction and play of puppets has led to the puppet theater having over 
+  15 puppet plays to its credit. Puppets at Dhaatu are crafted from wood and painted and finished with elaborate 
+  costuming and accurate stringing. The stylization of puppets is in-keeping with the traditional Moodalapaya 
+  Yakshagana style of Karnataka. The stringing techniques are improved versions of the pan-Indian control systems known. 
+  Dhaatu has, over time, developed to become India’s first puppetry brand name that catapulted puppetry into being 
+  recognized as a respected and classical art form of India.
+        </p>
+        <p className="text-xl font-bold">Dhaatu Team: Anupama and Vidyashankar Hoskere</p>
+        <p className="text-lg">
+        Anupama Hoskere is a master puppeteer and the Founder-Director of the Dhaatu Puppet Theater, Bengaluru, India. 
+        She is the recipient of the prestigious Sangeet Natak Akademi Puraskar, the highest Indian recognition given 
+        to practicing artists. She has wide experience in every facet of the composite art form of puppetry. 
+        A careful and detailed study of the Natyashastra provided Anupama much impetus to expand the intricacies 
+        and intellectual merit of her productions. Anupama received the Senior Research Fellowship from the 
+        Ministry of Culture which gave her an opportunity to redesign the old puppet theater into a more complex 
+        layered theater - combining dance, theater and puppetry into one Broadway scale performance of about 
+        80 minutes. Anupama is also the Festival Director of the Dhaatu International Puppet Festival – an 
+        annual festival in Bengaluru. 
+         </p>
+        <p className="text-lg">
+          Vidyashankar Hoskere is one of the founder Directors of Dhaatu. Vidyashankar has been a scriptwriter 
+          and artist of the Dhaatu Puppet Theater. He has funded and mentored the Epic Classes, Puppet Festivals 
+          and Doll displays at Dhaatu since its beginnings. He is an amateur cartoonist and does clay modeling. 
+          He has received formal training in the art of wood carving of puppets from Miroslav Trejtnar, a Czech master.
+        </p>
+       
+        <p className="text-xl font-bold">Synopsis of the play</p>
+        <p className="text-lg">
+        Mālavikāgnimitram is a romantic comedy set in the court of King Agnimitra of the Vidisha kingdom, belonging 
+  to the Shunga dynasty of the second century. Agnimitra, the hero of the play, is the son of Pushyamitra and father of 
+  Vasumitra. Malavika the Heroine, is the princess of the Vidarbha kingdom and the sister of prince Madhavasena. 
+  Since Prince Madhavasena’s kingdom has been annexed and he is in prison, his minister Sumathi escapes with 
+    Malavika and his own sister Kaushiki. But they are waylaid by robbers. Minister Sumathi is killed. Malavika and 
+    Kaushiki escape and end up in King Agnimitra’s court incognito. They are under the care of Queen Dharini, the 
+  mother of Prince Vasumitra. Iravati is the second wife of King Agnimitra. 
+       </p>
+  <p className="text-lg">
+  Another important character in the play is the Vidushaka or Court jester Gautama. Gautama is also the Sutradhara of the play. Gautama is the best friend and 
+  advisor of King Agnimitra. Gautama hatches out all the plots for King Agnimitra to meet his love Malavika 
+  and escape the wrath of queen Iravati and censure of queen Dharini. Poet Kalidasa beautifully portrays the 
+  character and unravels a plot full of fateful incidents with humorous angles.
+  </p>
+  <p className="text-lg">
+  Classical arts of music and dance and the established status of the fine arts are used as a big part of the story. Queen Dharini has a portrait Gallery where king Agnimitra sees Malavika’s portrait and falls in love. Gautama creates rivalry between the two dance teachers Ganadasa and Haradatta and succeeds in creating a dance competition between their students, thus bringing Malavika into the court of king Agnimitra.
+   </p>
+    <p className="text-lg">
+    The celebrations of the Vasanta Ritu play an important role in this play. The swing festival or the Dolotsava, is used as an opportunity for executing the plans of Vidushaka Gautama to make King Agnimitra and Malavika meet. The tradition of Dohada where the beautifully adorned feet of a beautiful maiden upon touching the golden Asoka tree causes it to blossom becomes the rendezvous of King Agnimitra and Malavika.
+    </p>
+ 
+    <p className="text-lg">
+    In another incident, King Pushyamitra accompanied by Grandson Vasumitra is following the Ashwamedha horse of the Vidisha kingdom. The horse is captured by the Yavanas or the Greeks who are trying to invade Bharata.  Queen Dharini is very proud of her son Vasumitra’s victory and valour and feels generous.
+    </p>
+    <p className="text-xl font-bold">Artist Credits and editorial metadata</p>
+    <ul className="list-disc text-lg list-inside">
+          <li>Directed by Anupama Hoskere</li>
+          <li>Dancer: Divya Hoskere as Malavika</li>
+          <li>Puppeteers: Anupama Hoskere, Mythilly Shikarpur, Chitra Rao, Divya Hoskere, Prakruti Hoskere </li>
+          <li>Music: Sri. Mahesh Swamy</li>
+          <li>Lyrics: Shatavadhani Dr. R. Ganesh</li>
+          <li>Dance choreography: Sri. P. Praveen Kumar</li>
+          <li>Genre: Musical string puppet show with special appearance of rod and shadow puppets</li>
+          <li>Language: English</li>
+          <li>Music Lyrics: Kannada</li>
+          <li>Dialogues: Anupama & Vidyashankar Hoskere</li>
+          <li>Duration: 75 min</li>
+    </ul>
+    </div>
+    </div>
+  );
+};
+
+export default Dhaatu;
