51f155d432ac50e859d181c3c688ac7915a4879b
Ajay Srinivasamurthy <ajays.murthy@upf.edu>
2022-11-05 13:19:06 +0530

    Create index.js

---
+++ src/pages/program/social/dhaatu/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../../../../components/layout'
+import Dhaatu from '../../../../components/dhaatu'
+
+const Index = () => {
+  return (
+    <Layout>
+      <div className="flex flex-col space-y-5 md:mx-30 lg:mx-52 mx-5 text-left ">
+        <Dhaatu/>
+      </div>
+    </Layout>
+  )
+}
+
+export default Index
