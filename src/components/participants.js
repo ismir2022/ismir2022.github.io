5d4755effd642016a77cd8db1162f6d630e2b395
Ajay Srinivasamurthy <ajays.murthy@upf.edu>
2022-11-24 00:29:20 +0530

    Update participants.js

--- src/components/participants.js
+++ src/components/participants.js
@@ -7,9 +7,36 @@ const Participants = () => {
         <h1 className="text-[#d83616] font-bold md:text-3xl text-2xl mt-5">
           Instructions for Participants
         </h1>
-        <p className="text-lg">
-          Coming Soon!
-        </p>
+    
+        <div className="space-y-3">
+          <p className="text-2xl font-bold">COVID-19 related guidelines for in-person participants</p>
+          <p className="text-lg"> Though local government regulations have relaxed COVID-19 related restrictions 
+            considerably, we require you to follow the below guidelines during your attendance 
+            at ISMIR 2022 for enhanced safety of participants. The conference will be taking all necessary precautions to avoid 
+            the incidence and spread of COVID-19. Any change in the local law/protocol due to COVID-19 
+             will be communicated at regular intervals as applicable. 
+          </p>
+          <p className="text-xl font-bold"> Guidelines </p>
+          <ul className="list-disc text-lg list-inside">
+            <li>Participants are requested to carry their COVID-19 vaccination certificates (if vaccinated) and show them during registration.</li>
+            <li>Participants are required to maintain social distancing during the conference. This will also be enforced by the conference staff and the volunteers.</li>
+            <li>Participants are required to wear well-fitting masks that fit completely over their nose and mouth.</li>
+            <li>Participants are requested to wash their hands frequently or use hand sanitizers</li>
+            <li>Participants are advised to stay home/hotel if they have tested positive for COVID-19, are waiting for COVID-19 test results, have COVID-19 symptoms, or if they have had close contact with a person who has tested positive for or who has symptoms of COVID-19</li>
+            <li>We discourage participants from greeting each other with physical contact (for example handshakes)</li>
+            <li>We request to use multiple entrances and exits and avoid crowding near waiting areas and dining areas</li> 
+            <li>Participants are requested to avoid touching surfaces</li>
+            <li>Participants are requested to follow state and local requirements and recommendations related to travel</li>
+            <li>Participants agree to assume any health risks related to their attendance</li>
+          </ul>
+          <p>Masks and necessary sanitizers for use by participants will be available at the conference venue. Should a participant needs to get tested for COVID-19, we will facilitate their RT-PCR or Rapid Antigen COVID-19 tests through trusted partners at the conference venue or the hotels where the participant is staying.</p>
+          <p>We seek your co-operation to comply with above guidelines to reduce the risk of incidence and spread of COVID-19 during ISMIR 2022 and maintain a safe and healthy environment for all participants of the conference.</p>
+        </div>
+        <div className="space-y-3">
+         <p className="text-lg">
+           More instructions coming Soon!
+         </p>
+        </div>
       </div>
     </div>
   );
