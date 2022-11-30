6a6aed8a0481d5a707b781445a6c092892f2029d
Siddharth Gururani <siddgururani@gatech.edu>
2022-11-30 02:41:50 -0800

    add lbd accepted, details

--- src/components/lbdprogram.js
+++ src/components/lbdprogram.js
@@ -18,7 +18,6 @@ const Latebreaking = () => {
       </div>
       
         <p className="text-xl text-[#d83616] font-bold">Hybrid Conference</p>
-        <p className="text-lg">
           Similar to the main paper track, this year’s LBD will be held in a hybrid format, i.e., physical and remote. 
           There will be two back-to-back sessions:
             <ol>
@@ -26,17 +25,160 @@ const Latebreaking = () => {
             <li>Virtual session: Dec 8, 2022 (Thu), 5.30 pm - 7.00 pm</li>
             </ol>
         </p>
+
+        <p className="text-xl text-[#d83616] font-bold">Instructions to participants/attendees</p>
+          Each LBD extended abstract will have a dedicated slack channel on the virtual platform and will have the abstract
+          and poster / video on MiniConf.Both in -person and remote attendees can ask asynchronous questions to remote or
+          in -person presenters via the slack channel.
+            <ol>
+            <li>Physical session: The LBD posters by in-person attendees will be presented during this session at the venue.
+                Virtual presenters can choose to remain online to interact with any interested participants.</li>
+            <li>Remote session: Remote presenters can use the huddle feature on each LBD extended abstract slack channel to
+                present their LBD posters on a video call. (Huddle supports up to 50 simultaneous participants).</li>
+            </ol >
+        </p >
   
         <p className="text-xl text-[#d83616] font-bold">Open Session</p>
-        <p className="text-lg">
           This year’s local LBD session is also an open session for students and independent researchers of the local community. 
           The session will enable participants to attend the late-breaking/demo session of the conference and interact with students, experts and senior members of the ISMIR research community. 
           This open session is a free registration event.
         </p>
         
         <p className="text-lg">
-          Accepted LBD submissions and detailed LBD program will be added shortly!
+          Accepted LBD submissions are below. More submissions to be added soon!
         </p>
+
+      <dl>
+        <div style={{paddingBottom:"10px"}}>
+        <dt><strong>Audio Augmentations for Semi-Supervised Learning with FixMatch</strong></dt>
+        <dd>Sascha Grollmisch, Estefanía Cano, Jakob Abeßer</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+        <dt><strong>libf0: A Python Library for Fundamental Frequency Estimation</strong></dt>
+        <dd>Sebastian Rosenzweig, Simon J Schwär, Meinard Müller</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+        <dt><strong>EGFxSet: ELECTRIC GUITAR TONES PROCESSED THROUGH REAL EFFECTS OF DISTORTION, MODULATION, DELAY AND REVERB</strong></dt>
+        <dd>Hegel Emmanuel Pedroza, Gerardo  Meza , Iran R Roman</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+        <dt><strong>Generating Melodies with Controllable Similarity and Length in ABC Notation</strong></dt>
+        <dd>Shangda Wu, Yuanliang Dong, Maosong Sun</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+        <dt><strong>Improving tokenization expressiveness with pitch intervals</strong></dt>
+        <dd>Louis Bigo, Mikaela Keller</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+        <dt><strong>Visualizing Chord Recognition Performance</strong></dt>
+        <dd>Christopher Liscio, Dan Brown</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+        <dt><strong>Towards modeling alternating patterns through inter-notes relations</strong></dt>
+        <dd>Perrine Vantalon, Mathieu Giraud, Richard Groult, Thierry Lecroq</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+        <dt><strong>Decoding and Visualising Intended Emotion in an Expressive Piano Performance</strong></dt>
+        <dd>Shreyan Chowdhury, Gerhard Widmer</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+        <dt><strong>Augmented Reality Visualization for Musical Instrument Learning</strong></dt>
+        <dd>Frank Heyen, Michael Sedlmair</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+        <dt><strong>AudioLoader: a hassle-free Pytorch audio dataset loader</strong></dt>
+        <dd>Kin Wai Cheuk, Kwan Yee Heung, Dorien Herremans</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+        <dt><strong>A Web-Based MIDI Controller for Music Live Coding</strong></dt>
+        <dd>Frank Heyen, Michael Sedlmair</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+        <dt><strong>Gamaka Synthesis for Kalpitha Swaras in Carnatic music</strong></dt>
+        <dd>Raghavasimhan Sankaranarayanan, Gil Weinberg</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+        <dt><strong>Ssm-Net: Feature Learning For Music Structure Analysis Using A Self-Similarity-Matrix Based Loss</strong></dt>
+        <dd>Geoffroy Peeters, Florian Angulo</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+        <dt><strong>Differentiating The Pitch Bend Function Between The Sitar As A Real & Virtual Instrument</strong></dt>
+        <dd>Suhit Chiruthapudi</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+        <dt><strong>ISMIR2022_LBD_Retnasree</strong></dt>
+        <dd>Retnasree Iyer</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+        <dt><strong>On the Typicality of Musical Sequences</strong></dt>
+        <dd>Mathais Rose Bjare, Stefan Lattner</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+        <dt><strong>Towards a machine-learning approach to analyse the emotional impact of source localization in music</strong></dt>
+        <dd>Eleonora De Filippi, Timothy Schmele, ARIJIT  NANDI</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+        <dt><strong>Rapidly Predicting Music Artistic Expression Preference From Heart Rate And Respiration Rate </strong></dt>
+        <dd>Shu Sakamoto, Vincent Cheung, Shinichi Furuya</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+        <dt><strong>MuSFA: Improving Music Structural Function Analysis with Partially Labeled Data</strong></dt>
+        <dd>Ju-Chiang Wang, Jordan B. L. Smith, Yun-Ning Hung</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+        <dt><strong>Audio Metaphor 2.0: An Improved System for Automatic Sound Design</strong></dt>
+        <dd>Renaud Bougueng Tchemeube, Joshua Kranabetter, Craig Carpenter, Philippe Pasquier, Miles Thorogood</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+        <dt><strong>Calliope: An Online Interface for Generative Music Co-Creation</strong></dt>
+        <dd>Renaud Bougueng Tchemeube, Jeffrey Ens, Cale Plut, Philippe Pasquier</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+        <dt><strong>Western Music Notation for Java: A library for music notation on the JVM</strong></dt>
+        <dd>Otso Björklund</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+        <dt><strong>Midi-Draw: Sketching To Control Melody Generation</strong></dt>
+        <dd>Tashi Namgyal</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+        <dt><strong>Melody Slot Machine HD</strong></dt>
+        <dd>Masatoshi Hamanaka</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+        <dt><strong>Differentiable WORLD Synthesizer-Based Neural Vocoder With Application To End-To-End Audio Style Transfer</strong></dt>
+        <dd>Shahan Nercessian</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+        <dt><strong>A New Pianoroll With Relative Pitch Approach</strong></dt>
+        <dd>Seonghyeon Go</dd>
+        </div>
+      </dl>
         
     </div>
   );
