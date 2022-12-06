6035c298bfa0c0f34a74d8776deccf3e15981d96
Siddharth Gururani <siddgururani@gatech.edu>
2022-12-06 10:28:50 -0800

    Add all accepted lbds

--- src/components/lbdprogram.js
+++ src/components/lbdprogram.js
@@ -53,133 +53,262 @@ const Latebreaking = () => {
 
       <dl>
         <div style={{paddingBottom:"10px"}}>
-        <dt><strong>Audio Augmentations for Semi-Supervised Learning with FixMatch</strong></dt>
-        <dd>Sascha Grollmisch, Estefanía Cano, Jakob Abeßer</dd>
+            <dt><strong>Hit Song Prediction for Indian Popular Music</strong></dt>
+            <dd>Shreya Kale, Makarand Velankar, Rameshwari Joshi, Vaishnavi Ingole, Aparna Dhaygude</dd>
         </div>
         
         <div style={{paddingBottom:"10px"}}>
-        <dt><strong>libf0: A Python Library for Fundamental Frequency Estimation</strong></dt>
-        <dd>Sebastian Rosenzweig, Simon J Schwär, Meinard Müller</dd>
+            <dt><strong>Audio Augmentations for Semi-Supervised Learning with FixMatch</strong></dt>
+            <dd>Sascha Grollmisch, Estefanía Cano, Jakob Abeßer</dd>
         </div>
         
         <div style={{paddingBottom:"10px"}}>
-        <dt><strong>EGFxSet: ELECTRIC GUITAR TONES PROCESSED THROUGH REAL EFFECTS OF DISTORTION, MODULATION, DELAY AND REVERB</strong></dt>
-        <dd>Hegel Emmanuel Pedroza, Gerardo  Meza , Iran R Roman</dd>
+            <dt><strong>libf0: A Python Library for Fundamental Frequency Estimation</strong></dt>
+            <dd>Sebastian Rosenzweig, Simon J Schwär, Meinard Müller</dd>
         </div>
         
         <div style={{paddingBottom:"10px"}}>
-        <dt><strong>Generating Melodies with Controllable Similarity and Length in ABC Notation</strong></dt>
-        <dd>Shangda Wu, Yuanliang Dong, Maosong Sun</dd>
+            <dt><strong>Genre Classification and Analysis of Marathi Songs</strong></dt>
+            <dd>Shreyas M Nadkarni, Preeti Rao</dd>
         </div>
         
         <div style={{paddingBottom:"10px"}}>
-        <dt><strong>Improving tokenization expressiveness with pitch intervals</strong></dt>
-        <dd>Louis Bigo, Mikaela Keller</dd>
+            <dt><strong>Towards Singer-independent Raga Classification from Audiovisual Recordings</strong></dt>
+            <dd>Adbhut V Bhardwaj, Sujoy Roychowdhury, Preeti Rao</dd>
         </div>
         
         <div style={{paddingBottom:"10px"}}>
-        <dt><strong>Visualizing Chord Recognition Performance</strong></dt>
-        <dd>Christopher Liscio, Dan Brown</dd>
+            <dt><strong>EGFxSet: Electric Guitar Tones Processed Through Real Effects of Distortion, Modulation, Delay and
+                    Reverb</strong></dt>
+            <dd>Hegel Emmanuel Pedroza, Gerardo Meza, Iran R Roman</dd>
         </div>
         
         <div style={{paddingBottom:"10px"}}>
-        <dt><strong>Towards modeling alternating patterns through inter-notes relations</strong></dt>
-        <dd>Perrine Vantalon, Mathieu Giraud, Richard Groult, Thierry Lecroq</dd>
+            <dt><strong>Generating Melodies with Controllable Similarity and Length in ABC Notation</strong></dt>
+            <dd>Shangda Wu, Yuanliang Dong, Maosong Sun</dd>
         </div>
         
         <div style={{paddingBottom:"10px"}}>
-        <dt><strong>Decoding and Visualising Intended Emotion in an Expressive Piano Performance</strong></dt>
-        <dd>Shreyan Chowdhury, Gerhard Widmer</dd>
+            <dt><strong>Improving tokenization expressiveness with pitch intervals</strong></dt>
+            <dd>Louis Bigo, Mikaela Keller</dd>
         </div>
         
         <div style={{paddingBottom:"10px"}}>
-        <dt><strong>Augmented Reality Visualization for Musical Instrument Learning</strong></dt>
-        <dd>Frank Heyen, Michael Sedlmair</dd>
+            <dt><strong>Visualizing Chord Recognition Performance</strong></dt>
+            <dd>Christopher Liscio, Dan Brown</dd>
         </div>
         
         <div style={{paddingBottom:"10px"}}>
-        <dt><strong>AudioLoader: a hassle-free Pytorch audio dataset loader</strong></dt>
-        <dd>Kin Wai Cheuk, Kwan Yee Heung, Dorien Herremans</dd>
+            <dt><strong>Towards modeling alternating patterns through inter-notes relations</strong></dt>
+            <dd>Perrine Vantalon, Mathieu Giraud, Richard Groult, Thierry Lecroq</dd>
         </div>
         
         <div style={{paddingBottom:"10px"}}>
-        <dt><strong>A Web-Based MIDI Controller for Music Live Coding</strong></dt>
-        <dd>Frank Heyen, Michael Sedlmair</dd>
+            <dt><strong>Exploring Popularity Bias in Music Steaming Services</strong></dt>
+            <dd>Douglas Turnbull, Vera Crabtree, Sean McQuillan</dd>
         </div>
         
         <div style={{paddingBottom:"10px"}}>
-        <dt><strong>Gamaka Synthesis for Kalpitha Swaras in Carnatic music</strong></dt>
-        <dd>Raghavasimhan Sankaranarayanan, Gil Weinberg</dd>
+            <dt><strong>Decoding and Visualising Intended Emotion in an Expressive Piano Performance</strong></dt>
+            <dd>Shreyan Chowdhury, Gerhard Widmer</dd>
         </div>
         
         <div style={{paddingBottom:"10px"}}>
-        <dt><strong>Ssm-Net: Feature Learning For Music Structure Analysis Using A Self-Similarity-Matrix Based Loss</strong></dt>
-        <dd>Geoffroy Peeters, Florian Angulo</dd>
+            <dt><strong>CCVS: A dataset for concert videography research</strong></dt>
+            <dd>Hsin-Min Chou, Ting-Wei Lin, Jen-Chun Lin, Ching Te Chiu, Li Su</dd>
         </div>
         
         <div style={{paddingBottom:"10px"}}>
-        <dt><strong>Differentiating The Pitch Bend Function Between The Sitar As A Real & Virtual Instrument</strong></dt>
-        <dd>Suhit Chiruthapudi</dd>
+            <dt><strong>Augmented Reality Visualization for Musical Instrument Learning</strong></dt>
+            <dd>Frank Heyen, Michael Sedlmair</dd>
         </div>
         
         <div style={{paddingBottom:"10px"}}>
-        <dt><strong>ISMIR2022_LBD_Retnasree</strong></dt>
-        <dd>Retnasree Iyer</dd>
+            <dt><strong>AudioLoader: a hassle-free Pytorch audio dataset loader</strong></dt>
+            <dd>Kin Wai Cheuk, Kwan Yee Heung, Dorien Herremans</dd>
         </div>
         
         <div style={{paddingBottom:"10px"}}>
-        <dt><strong>On the Typicality of Musical Sequences</strong></dt>
-        <dd>Mathais Rose Bjare, Stefan Lattner</dd>
+            <dt><strong>A Web-Based MIDI Controller for Music Live Coding</strong></dt>
+            <dd>Frank Heyen, Michael Sedlmair</dd>
         </div>
         
         <div style={{paddingBottom:"10px"}}>
-        <dt><strong>Towards a machine-learning approach to analyse the emotional impact of source localization in music</strong></dt>
-        <dd>Eleonora De Filippi, Timothy Schmele, ARIJIT  NANDI</dd>
+            <dt><strong>Gamaka Synthesis for Kalpitha Swaras in Carnatic music</strong></dt>
+            <dd>Raghavasimhan Sankaranarayanan, Gil Weinberg</dd>
         </div>
         
         <div style={{paddingBottom:"10px"}}>
-        <dt><strong>Rapidly Predicting Music Artistic Expression Preference From Heart Rate And Respiration Rate </strong></dt>
-        <dd>Shu Sakamoto, Vincent Cheung, Shinichi Furuya</dd>
+            <dt><strong>SSM-NET: Feature Learning for Music Structure Analysis Using a Self-similarity-matrix Based
+                    Loss</strong></dt>
+            <dd>Geoffroy Peeters, Florian Angulo</dd>
         </div>
         
         <div style={{paddingBottom:"10px"}}>
-        <dt><strong>MuSFA: Improving Music Structural Function Analysis with Partially Labeled Data</strong></dt>
-        <dd>Ju-Chiang Wang, Jordan B. L. Smith, Yun-Ning Hung</dd>
+            <dt><strong>Differentiating the Pitch Bend Function Between the Sitar as a Real and Virtual Instrument</strong></dt>
+            <dd>Suhit Chiruthapudi</dd>
         </div>
         
         <div style={{paddingBottom:"10px"}}>
-        <dt><strong>Audio Metaphor 2.0: An Improved System for Automatic Sound Design</strong></dt>
-        <dd>Renaud Bougueng Tchemeube, Joshua Kranabetter, Craig Carpenter, Philippe Pasquier, Miles Thorogood</dd>
+            <dt><strong>Improvisation of tabla the indian percussion using fibre</strong></dt>
+            <dd>Retnasree Iyer</dd>
         </div>
         
         <div style={{paddingBottom:"10px"}}>
-        <dt><strong>Calliope: An Online Interface for Generative Music Co-Creation</strong></dt>
-        <dd>Renaud Bougueng Tchemeube, Jeffrey Ens, Cale Plut, Philippe Pasquier</dd>
+            <dt><strong>On the Typicality of Music</strong></dt>
+            <dd>Mathais Rose Bjare, Stefan Lattner</dd>
         </div>
         
         <div style={{paddingBottom:"10px"}}>
-        <dt><strong>Western Music Notation for Java: A library for music notation on the JVM</strong></dt>
-        <dd>Otso Björklund</dd>
+            <dt><strong>Towards a machine-learning approach to analyse the emotional impact of source localization in
+                    music</strong></dt>
+            <dd>Eleonora De Filippi, Timothy Schmele, Arijit Nandi</dd>
         </div>
         
         <div style={{paddingBottom:"10px"}}>
-        <dt><strong>Midi-Draw: Sketching To Control Melody Generation</strong></dt>
-        <dd>Tashi Namgyal</dd>
+            <dt><strong>A CHORD PROGRESSION LIBRARY FOR MEASURING EMOTIONS BY BCIs</strong></dt>
+            <dd>Raffaella Folgieri, Enrico Daga, Claudio Lucchiari, Paul Arnold</dd>
         </div>
         
         <div style={{paddingBottom:"10px"}}>
-        <dt><strong>Melody Slot Machine HD</strong></dt>
-        <dd>Masatoshi Hamanaka</dd>
+            <dt><strong>Rapidly Predicting Music Artistic Expression Preference From Heart Rate and Respiration Rate</strong>
+            </dt>
+            <dd>Shu Sakamoto, Vincent Cheung, Shinichi Furuya</dd>
         </div>
         
         <div style={{paddingBottom:"10px"}}>
-        <dt><strong>Differentiable WORLD Synthesizer-Based Neural Vocoder With Application To End-To-End Audio Style Transfer</strong></dt>
-        <dd>Shahan Nercessian</dd>
+            <dt><strong>MuSFA: Improving Music Structural Function Analysis with Partially Labeled Data</strong></dt>
+            <dd>Ju-Chiang Wang, Jordan B. L. Smith, Yun-Ning Hung</dd>
         </div>
         
         <div style={{paddingBottom:"10px"}}>
-        <dt><strong>A New Pianoroll With Relative Pitch Approach</strong></dt>
-        <dd>Seonghyeon Go</dd>
+            <dt><strong>Audio Metaphor 2.0: An Improved System for Automatic Sound Design</strong></dt>
+            <dd>Renaud Bougueng Tchemeube, Joshua Kranabetter, Craig Carpenter, Philippe Pasquier, Miles Thorogood</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+            <dt><strong>Calliope: An Online Interface for Generative Music Co-Creation</strong></dt>
+            <dd>Renaud Bougueng Tchemeube, Jeffrey Ens, Cale Plut, Philippe Pasquier</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+            <dt><strong>Western Music Notation for Java: A library for music notation on the JVM</strong></dt>
+            <dd>Otso Björklund</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+            <dt><strong>A Multimodal Approach to Acoustic Guitar Strumming Action Transcription</strong></dt>
+            <dd>Sebastian Murgul, Michael Heizmann</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+            <dt><strong>Midi-Draw: Sketching To Control Melody Generation</strong></dt>
+            <dd>Tashi Namgyal</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+            <dt><strong>AI-driven, mobile-first web UI for controllable expressive piano performance composition</strong></dt>
+            <dd>Théis Bazin, Gaëtan Hadjeres, Mikhail Malt</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+            <dt><strong>Beatoven.ai: AI-powered music creation</strong></dt>
+            <dd>António Ramires, Siddharth Bhardwaj</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+            <dt><strong>Facets: A Tool For Management And Navigation Of Symbolic Music Collections</strong></dt>
+            <dd>Tiange Zhu, Raphael Fournier-S'niehotta</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+            <dt><strong>Melody Slot Machine HD</strong></dt>
+            <dd>Masatoshi Hamanaka</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+            <dt><strong>Assistive alignment of in-the-wild sheet music and performances</strong></dt>
+            <dd>Michael Feffer, Chris Donahue, Zachary Lipton</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+            <dt><strong>Note level MIDI velocity estimation for piano performance</strong></dt>
+            <dd>Hyon Kim, Marius Miron, Xavier Serra</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+            <dt><strong>Essentia API: a web API for music audio analysis</strong></dt>
+            <dd>Albin Andrew Correya, Dmitry Bogdanov, Pablo Alonso-Jiménez, Xavier Serra</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+            <dt><strong>Differentiable WORLD Synthesizer-Based Neural Vocoder With Application To End-To-End Audio Style
+                    Transfer</strong></dt>
+            <dd>Shahan Nercessian</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+            <dt><strong>Schmubert: A Symbolic Creative Harmonic Music Unmasking Bidirectional Encoder Representation
+                    Transformer</strong></dt>
+            <dd>Matthias Plasser, Silvan Peter</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+            <dt><strong>Song Describer: a Platform for Collecting Textual Descriptions of Music Recordings</strong></dt>
+            <dd>Ilaria Manco, Benno Weck, Philip Tovstogan, Minz Won, Dmitry Bogdanov</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+            <dt><strong>A Framework for Instrument Recognition Using Conditional Generative Adversarial Networks</strong></dt>
+            <dd>Charis Cochran, Youngmoo Kim</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+            <dt><strong>Differentiable Acoustic Guitar Synthesis from Tablature</strong></dt>
+            <dd>Andrew F Wiggins, Youngmoo Kim</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+            <dt><strong>A New Pianoroll With Relative Pitch Approach</strong></dt>
+            <dd>Seonghyeon Go</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+            <dt><strong>Unsupervised Domain Adaptation For Sound Event Detection In Music Applications</strong></dt>
+            <dd>Arkaprava Biswas, Akshay Raina, Vipul Arora</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+            <dt><strong>MAP-Music2Vec: A Simple and Effective Baseline for Self-Supervised Music Audio Representation
+                    Learning</strong></dt>
+            <dd>Yizhi Li, Ruibin Yuan, Ge Zhang, Yinghao MA, Chenghua Lin, Xingran Chen, Anton Ragni, Hanzhi Yin, Zhijie Hu,
+                Haoyu He, Emmanouil Benetos, Norbert Gyenge,Ruibo Liu, Jie Fu</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+            <dt><strong>CogXAI ANNalyzer: Cognitive Neuroscience Inspired Techniques for eXplainable AI</strong></dt>
+            <dd>Maral Ebrahimzadeh, Valerie Krug, Sebastian Stober</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+            <dt><strong>A Family of Libraries for Working With Pitches and Intervals</strong></dt>
+            <dd>Christoph Finkensiep, Robert Lieck, Martin A Rohrmeier</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+            <dt><strong>Audio Latent Space Cartography</strong></dt>
+            <dd>Nicolas Jonason, Bob Sturm</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+            <dt><strong>DiscStitch: towards audio-to-audio alignment with robustness to playback speed variabilities</strong>
+            </dt>
+            <dd>Joren Six</dd>
+        </div>
+        
+        <div style={{paddingBottom:"10px"}}>
+            <dt><strong>Neural Waveform Synthesis on a Low-Resource Embedded Device</strong></dt>
+            <dd>Alexander Tipper</dd>
         </div>
       </dl>
         
